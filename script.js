71924f52fceb10aeb965951c201eec0402edc92a
Neoob <gabrielngc@gmail.com>
2022-10-10 21:20:23 -0300

    display weather

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 let weather = {
   "apikey": "8dade0dd7e675bf4a3a69f2330890fcd",
   fetchWeather: function (city) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apikey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pt_br&appid=${this.apikey}`)
       .then((respnse) => respnse.json())
-      .then((data) => console.log(data));
+      .then((data) => this.displayWeather(data));
   },
   displayWeather: function (data) {
+    const { name } = data;
+    const { description, icon } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    const iconurl = `http://openweathermap.org/img/w/${icon}.png"`;
 
-  }
+    //display weather
+    document.querySelector('.city').innerText = `${name}`;
+    document.querySelector('h1').innerText = `${temp}°C`;
+    document.querySelector('.description').innerText = description;
+    document.querySelector('.icon').src = iconurl;
+    document.querySelector('.humnidity').innerText = `Umidade: ${humidity}%`;
+    document.querySelector('.wind').innerText = `Velocidade do vento: ${speed}km/h`
+
+
+    document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${name}')`;
+  },
 
 }
 const searchBar = document.querySelector('.search-bar');
@@ -21,3 +36,6 @@ searchBtn.addEventListener('click', ((e) => {
   console.log(city)
   weather.fetchWeather(searchBar.value);
 }))
+
+
+weather.fetchWeather('Brasil');
